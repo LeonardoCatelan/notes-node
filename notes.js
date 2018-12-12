398a4bb2dd4eaa76d94730ca6283d25131505b3e
LeonardoCatelan <44733148+LeonardoCatelan@users.noreply.github.com>
2018-12-12 08:42:25 -0200

    Tirando 'debugging'

--- notes.js
+++ notes.js
@@ -53,7 +53,6 @@ var removeNotes = (title) => {
 };
 
 var logNote = (note) => {
-    debugger;
     console.log('---');
     console.log(`Title: ${note.title}`);
     console.log(`Body: ${note.body}`);
